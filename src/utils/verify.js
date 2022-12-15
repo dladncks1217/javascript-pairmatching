cd2334fe540cff9088879c5981e59e29039776d0
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-15 14:51:27 +0900

    feat(verify pairData): 페어매칭 입력 데이터 유효 여부 유틸함수 추가

--- src/utils/verify.js
+++ src/utils/verify.js
@@ -4,6 +4,11 @@ const verify = {
   inputTypeNumberOrQ(input) {
     if (input.toUpperCase() !== 'Q' && Number.isNaN(Number(input))) throw new Error();
   },
+
+  pairData(input) {
+    const result = input.split(',');
+    if (result.length !== 3) throw new Error();
+  },
 };
 
 module.exports = verify;
